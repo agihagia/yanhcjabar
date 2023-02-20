6f2437b4073d5635e7f6d565a88f681621c675b5
agi aditya <agi.aditya@pln.co.id>
2023-02-20 08:56:18 +0700

    menu

--- app/Views/front/agenda/vaksin.php
+++ app/Views/front/agenda/vaksin.php
@@ -1,19 +1,19 @@
 <style type="text/css">
-    .footer {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-    }
+  .footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 </style>
 
 <?= $this->extend('front/layout/main') ?>
 <?= $this->section('mainbg') ?>
 <!-- ======= Breadcrumbs ======= -->
 <div class="breadcrumbs" data-aos="fade-in">
-    <div class="container">
-        <h2>VAKSIN</h2>
-    </div>
+  <div class="container">
+    <h2>VAKSIN</h2>
+  </div>
 </div><!-- End Breadcrumbs -->
 
 <?= $this->endSection('mainbg') ?>
@@ -21,72 +21,38 @@
 
 <!-- ======= About Section ======= -->
 <section id="about" class="about">
-    <div class="container" data-aos="fade-up">
+  <div class="container" data-aos="fade-up">
 
-        <div class="row">
-            <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content">
-                <h3>COMING SOON</h3>
-            </div>
-            <!-- ======= Frequently Asked Questions Section ======= -->
-        </div>
-
+    <div class="row">
+      <div class="col-lg-12 pt-4 pt-lg-0 order-2 order-lg-1 content">
+        <b><p>Yth. Bapak dan Ibu</p>
+        <p> Seluruh Pegawai dan TAD</p>
+        <p> PLN KOTA BANDUNG</p></b>
+        <br>
+        <p> dengan ini kami turut mengundang bpk/ibu utk mendaftar Vaksin Booster ke 2 dengan Jenis Pfizer bagi yang bersedia untuk di berikan Vaksin Booster.</p>
+        <p>  Link Pendaftaran Sbb :</p>
+        <br>
+       <b> <p> <a href="bit.ly/PendaftaranVaksinBooster2PLNUP3Bandung" target="_blank">bit.ly/PendaftaranVaksinBooster2PLNUP3Bandung</a></p>
+        <br>    
+        <p> Hari : Rabu</p>
+        <p style="color:red;"> Tanggal : 22 Feb 2023</p>
+        <p> waktu : 08.00 sd Selesai</p>
+        <p> Tempat : GSBO_UP3 Bandung</p>
+        <p>Jl. Soekarno Hatta No.436, Ciseureuh, Kec. Regol, Kota Bandung, Jawa Barat 40255</p></b>
+        <br>
+        <p>  Demikian yang dapat disampaikan, di ucapkan terima kasih</p>
+        <br>
+        <p>  Salam</p>
+        <p> K3L dan Kam UP3 Bandung</p>
+        <br>
+        <b><p style="color:red;"> Note :</p>
+        <p style="color:red;"> - Untuk Boster 1 menggunakan Sinopharm belum bisa di akomodir dengan Pfizer.</p>
+        <p style="color:red;"> - Pendaftaran di tutup sd hari Senin tanggal 20 feb 2023 Pukul 13.00 WIB</p></b>
+        </p>
+      </div>
+      <!-- ======= Frequently Asked Questions Section ======= -->
     </div>
+
+  </div>
 </section><!-- End About Section -->
-<section id="faq" class="faq">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Frequently Asked Questions</h2>
-        </div>
-
-        <div class="faq-list" data-aos="zoom-in" data-aos-delay="100">
-        <div id="accordion">
-        <div class="card">
-          <div class="card-header" id="headingOne">
-            <h5 class="mb-0">
-              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Collapsible Group Item #1
-              </button>
-            </h5>
-          </div>
-
-          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-            <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header" id="headingTwo">
-            <h5 class="mb-0">
-              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Collapsible Group Item #2
-              </button>
-            </h5>
-          </div>
-          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-            <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header" id="headingThree">
-            <h5 class="mb-0">
-              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Collapsible Group Item #3
-              </button>
-            </h5>
-          </div>
-          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-            <div class="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-          </div>
-        </div>
-      </div>
-        </div>
-
-      </div>
-    </section><!-- End Frequently Asked Questions Section -->
 <?= $this->endSection('isi') ?>
