bb388090eb68d752b764fd623a3dd4eba29f6811
agi aditya <agi.aditya@pln.co.id>
2023-03-04 17:02:48 +0700

    add provider

--- app/Views/front/layout/main.php
+++ app/Views/front/layout/main.php
@@ -154,12 +154,25 @@
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
-    <!--script src="<?= base_url() ?>/assets/vendor/purecounter/purecounter_vanilla.js"></!--script>
-    <!--script src="<?= base_url() ?>/assets/vendor/swiper/swiper-bundle.min.js"></!--script>
-    <!--script src="<?= base_url() ?>/assets/vendor/php-email-form/validate.js"></!--script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/php-email-form/validate.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/counterup/counterup.min.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="<?= base_url() ?>/assets/front/assets/vendor/aos/aos.js"></script>
 
-  <!-- Template Main JS File -->
-    <!--script src="assets/js/main.js"></!--script-->
+    <!-- Template Main JS File -->
+    <script src="<?= base_url() ?>/assets/front/assets/js/main.js"></script>
+    <script>
+        function numberOnly(event) {
+            var angka = (event.which) ? event.which : event.keyCode
+            if (angka != 46 && angka > 31 && (angka < 48 || angka > 57))
+                return false;
+            return true;
+        }
+    </script>
 </body>
 
 </html>
