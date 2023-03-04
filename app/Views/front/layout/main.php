2ecbfe4038f1255eae7d89d0a9c741108ca4de3b
agi aditya <agi.aditya@pln.co.id>
2023-03-04 17:05:56 +0700

    add provider

--- app/Views/front/layout/main.php
+++ app/Views/front/layout/main.php
@@ -153,16 +153,6 @@
     <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
     <div id="preloader"></div>
 
-    <!-- Vendor JS Files -->
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/php-email-form/validate.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/counterup/counterup.min.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-    <script src="<?= base_url() ?>/assets/front/assets/vendor/aos/aos.js"></script>
-
     <!-- Template Main JS File -->
     <script src="<?= base_url() ?>/assets/front/assets/js/main.js"></script>
     <script>
